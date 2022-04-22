902859080553d0fcb06b9669f50ac9a6d5d2e3db
Peter Fletcher <peterfletcher97@gmail.com>
2022-04-21 20:22:52 -0600

    added oauth refresh/access token generation and removed debugging landing page

--- enviro_variables.py
+++ enviro_variables.py
@@ -3,10 +3,10 @@ import os
 # URL that the json POST will be sent to
 SEND_URL = "https://webhook.site/9a227568-3994-4c29-9006-0762865b808b"
 
-# Email and email passwords
+# email and email passwords
 FROM_EMAIL = os.environ.get('GEAR_EMAIL')
 FROM_PASS = os.environ.get('GEAR_PASS')
-TO_EMAIL = "9a227568-3994-4c29-9006-0762865b808b@email.webhook.site"
+TO_EMAIL = FROM_EMAIL
 
 # Oauth tokens
 CLIENT_ID = os.environ.get('CLIENT_ID')
