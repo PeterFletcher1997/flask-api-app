c6958b318e51fc31d7fc38697db229a43b60e851
Peter Fletcher <peterfletcher97@gmail.com>
2022-06-30 19:58:01 -0600

    /update webhook url

--- enviro_variables.py
+++ enviro_variables.py
@@ -1,7 +1,7 @@
 import os
 
 # URL that the json POST will be sent to
-SEND_URL = "https://webhook.site/9a227568-3994-4c29-9006-0762865b808b"
+SEND_URL = "https://webhook.site/d02a90c7-71ad-45f8-9ba4-6acddc30887e"
 
 # email and email passwords
 FROM_EMAIL = os.environ.get('GEAR_EMAIL')
